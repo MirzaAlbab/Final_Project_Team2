5183f35563e2e77fb218acbd3bd763859203516e
kevin siregar <kevinsiregar22>
2022-06-20 14:48:24 +0700

    change fonts

--- src/utils/fonts/index.js
+++ src/utils/fonts/index.js
@@ -1,10 +1,11 @@
 export const fonts = {
-  Inter: {
-    300: 'Inter-Light',
-    400: 'Inter-Regular',
-    500: 'Inter-Medium',
-    600: 'Inter-SemiBold',
-    700: 'Inter-Bold',
-    800: 'Inter-ExtraBold',
+  Poppins: {
+    300: 'Poppins-Light',
+    400: 'Poppins-Regular',
+    500: 'Poppins-Medium',
+    600: 'Poppins-SemiBold',
+    700: 'Poppins-Bold',
+    800: 'Poppins-ExtraBold',
+    900: 'Poppins-Black',
   },
 };
