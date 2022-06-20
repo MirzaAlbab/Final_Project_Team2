bd47406e6cd2a8d71045bdc2ba7bd92612f588d8
kevin siregar <kevinsiregar22>
2022-06-20 12:52:53 +0700

    config colors

---
+++ src/utils/colors/index.js
@@ -0,0 +1,21 @@
+const mainColor = {
+  white: '#FFFFFF',
+  purple: '#7126B5',
+  purpleLight: '#E2D4F0',
+  green: '#73CA5C',
+  orange: '#FFE9C9',
+};
+
+export const colors = {
+  background: {
+    primary: mainColor.white,
+    secondary: mainColor.orange,
+  },
+  button: {
+    active: mainColor.purple,
+    disable: mainColor.purpleLight,
+  },
+  alert: {
+    green: mainColor.green,
+  },
+};
