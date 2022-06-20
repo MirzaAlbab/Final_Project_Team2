83b7249acef099969362c1b5ae6c9168d8b0e18c
kevin siregar <kevinsiregar22>
2022-06-20 13:31:58 +0700

    config fonts Inter

---
+++ src/utils/fonts/index.js
@@ -0,0 +1,10 @@
+export const fonts = {
+  Inter: {
+    300: 'Inter-Light',
+    400: 'Inter-Regular',
+    500: 'Inter-Medium',
+    600: 'Inter-SemiBold',
+    700: 'Inter-Bold',
+    800: 'Inter-ExtraBold',
+  },
+};
