ce81f30eec08a0c07b93dbca0050a95307fc49a8
kevin siregar <kevinsiregar22>
2022-06-22 09:56:21 +0700

    start

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import {API_URL} from '@env';
 export default function App() {
   return (
     <View>
-      <Text>{API_URL}</Text>
+      <Text>register</Text>
     </View>
   );
 }
