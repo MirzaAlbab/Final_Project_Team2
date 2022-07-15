c12222fe68a3c4b8635d9f8794f7a09ef9edcca7
Irfan Zidny <irfanzidny116@gmail.com>
2022-07-15 08:08:22 +0700

    Pengerjaan Forgot Password

---
+++ src/screens/ForgotPassword/index.js
@@ -0,0 +1,24 @@
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
+import React from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {Gap} from '../../components';
+import Headers from '../../components/Headers';
+
+const ForgotPassword = () => {
+  return (
+    <View>
+      <Text>ForgotPassword</Text>
+    </View>
+  );
+};
+
+export default ForgotPassword;
+
+const styles = StyleSheet.create({});
