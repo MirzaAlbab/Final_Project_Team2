4af62cec774c00a798077e47a91c727d9ad56b2e
unknown <mirzaalbabfirdaus17@gmail.com>
2022-07-21 22:14:12 +0700

    update daftar jual screen

--- src/screens/DaftarJual/index.js
+++ src/screens/DaftarJual/index.js
@@ -3,10 +3,9 @@ import React from 'react';
 import {ms} from 'react-native-size-matters';
 import Headers from '../../components/Headers';
 import {COLORS, fonts} from '../../utils';
-import CardBarangInfo from '../../components/Card/CardBarangInfo';
-import CardInfoWithImage from '../../components/Card/CardInfoWithImage';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import people from '../../assets/images/people.png';
 const DaftarJual = () => {
   return (
     <View style={styles.container}>
@@ -14,7 +13,7 @@ const DaftarJual = () => {
       <View style={styles.card}>
         <View style={styles.onContainer}>
           <Image
-            source={{uri: 'https://picsum.photos/200'}}
+            source={{uri: 'https://source.unsplash.com/random'}}
             resizeMode={'cover'}
             style={styles.peopleImage}
           />
@@ -29,9 +28,21 @@ const DaftarJual = () => {
           </View>
         </View>
       </View>
-      <View style={styles.rectangle}>
-        <Ionicons style={styles.addicon} name="add" size={30} />
-        <Text style={styles.icontext}>Add Image</Text>
+      <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+        <View style={styles.rectangle}>
+          <Ionicons style={styles.addicon} name="add" size={30} />
+          <Text style={styles.icontext}>Add Image</Text>
+        </View>
+        <View style={styles.productcard}>
+          <Image
+            source={{uri: 'https://source.unsplash.com/random/140x100'}}
+            resizeMode={'cover'}
+            style={styles.productImage}
+          />
+          <Text style={styles.productname}>Jam Tangan Casio</Text>
+          <Text style={styles.productcategory}>Aksesoris</Text>
+          <Text style={styles.productprice}>Rp 250.000</Text>
+        </View>
       </View>
     </View>
   );
@@ -123,10 +134,8 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     borderStyle: 'dotted',
-    borderRadius: 20,
+    borderRadius: 5,
     marginBottom: ms(10),
-    marginLeft: ms(10),
-    marginRight: ms(10),
   },
   addicon: {
     marginTop: ms(70),
@@ -135,6 +144,65 @@ const styles = StyleSheet.create({
   icontext: {
     marginBottom: ms(50),
     alignSelf: 'center',
+  },
+  productImage: {
+    height: ms(100),
+    width: ms(140),
+    borderRadius: ms(10),
+    alignSelf: 'center',
+    marginTop: ms(10),
+    marginBottom: ms(10),
+    marginLeft: ms(10),
+    marginRight: ms(10),
+  },
+  productname: {
+    fontFamily: fonts.Poppins['500'],
+    lineHeight: ms(14),
+    color: COLORS.darkBrown,
+    fontSize: ms(12),
+    alignSelf: 'stretch',
+
+    marginTop: ms(5),
+    marginBottom: ms(5),
+    marginLeft: ms(15),
+    marginRight: ms(5),
+  },
+  productcategory: {
+    fontFamily: fonts.Poppins['400'],
+    lineHeight: ms(14),
+    color: COLORS.gray,
+    fontSize: ms(12),
+    alignSelf: 'stretch',
+
+    marginTop: ms(5),
+    marginBottom: ms(5),
+    marginLeft: ms(15),
+    marginRight: ms(5),
+  },
+  productprice: {
+    fontFamily: fonts.Poppins['500'],
+    lineHeight: ms(14),
+    color: COLORS.darkBrown,
+    fontSize: ms(12),
+    alignSelf: 'stretch',
+    marginTop: ms(5),
+    marginBottom: ms(5),
+    marginLeft: ms(15),
+    marginRight: ms(5),
+  },
+  productcard: {
+    marginTop: ms(10),
+    marginBottom: ms(10),
+    borderRadius: ms(10),
+    backgroundColor: COLORS.white,
+    shadowColor: 'rgba(0, 0, 0, 0.15)',
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.14,
+    shadowRadius: 4.84,
+    elevation: 5,
   },
 });
 
