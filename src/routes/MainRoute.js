342fba70794fcd75db148d2c265cf42dc74a3d19
kevin siregar <kevinsiregar22>
2022-07-22 14:21:37 +0700

    fix UI Home

--- src/routes/MainRoute.js
+++ src/routes/MainRoute.js
@@ -12,9 +12,7 @@ const Stack = createStackNavigator();
 export default function MainRoute() {
   return (
     <Stack.Navigator
-
-      initialRouteName="Dashboard"
-
+      initialRouteName="Login"
       screenOptions={{headerShown: false}}>
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Register" component={Register} />
